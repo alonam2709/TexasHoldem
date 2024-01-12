4e2cc43f7d5ee9b0e628cfd035f700c2a56121e3
alonam2709 <alonam@my.ofs.edu.sg>
2024-01-12 11:02:22 +0800

    Initial commit

---
+++ src/com/timiles/tesaxholdem/Probability.java
@@ -0,0 +1,18 @@
+package com.timiles.tesaxholdem;
+
+public class Probability {
+
+	public int hand;
+	public double probability;
+	
+	public Probability(int hand, double probability) {
+		
+		if (!Hand.isValid(hand)) System.out.println(
+				"ERROR : Probability() invalid hand="+hand);
+		if (probability<0 || probability>1) System.out.println(
+				"ERROR : Probability() invalid probability="+probability);
+		
+		this.hand = hand;
+		this.probability = probability;
+	}
+}
---
+++ src/com/timiles/tesaxholdem/myUtils/MyMath.java
@@ -0,0 +1,26 @@
+
+
+package com.timiles.tesaxholdem.myUtils;
+
+
+public class MyMath {
+
+	public static int nCr(int n, int r) {
+		if (n<r) {
+			System.out.println("n less than r");
+			return -1; // throw exception
+		}
+		
+		return MyMath.factorial(n)/
+			( MyMath.factorial(n-r) * MyMath.factorial(r) );
+	}
+	
+	public static int factorial(int x) {
+		if (x<=0) return 1;
+		
+		int product = 1;
+		while (x>0) product *= x--;
+		
+		return product;
+	}
+}
